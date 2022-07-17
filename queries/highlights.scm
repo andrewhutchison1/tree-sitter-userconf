679e2e4393f4a1f52e46a7eff3bfb6bf3288faf5
Andrew <andrewhutchison@outlook.com.au>
2022-07-17 17:54:59 +1000

    implement basic syntax highlighting

---
+++ queries/highlights.scm
@@ -0,0 +1,12 @@
+; Comments
+(comment) @comment
+
+; Strings in key-position
+(record_entry key: [(unquoted_string) (quoted_string)] @keyword)
+
+; Strings elsewhere
+[(unquoted_string) (quoted_string) (multiline_string)] @string
+
+; Punctuation and delimiters
+["{" "}" "[" "]"] @punctuation.bracket
+[":" ","] @punctuation.delimiter
