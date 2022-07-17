51e14802208f4d3698393e66c77b7c8b32083596
Andrew <andrewhutchison@outlook.com.au>
2022-07-17 14:38:21 +1000

    remove multiline strings for now

--- grammar.js
+++ grammar.js
@@ -2,35 +2,34 @@ module.exports = grammar({
     name: 'Userconf',
 
     extras: $ => [
-        /\s/,
         $.comment,
+        /\s/,
     ],
 
     rules: {
         document: $ => optional($._record_body),
 
-        record:         $ => seq('{', optional($._record_body), '}'),
-        _record_body:   $ => separated1($, $.record_entry),
-        record_entry:   $ => seq($.key, $.value),
-        key:            $ => $._str_key,
-        value:          $ => choice($._str_any, $.record, $.list),
+        record:             $ => seq('{', optional($._record_body), '}'),
+        _record_body:       $ => separated1($.record_entry, $._separator),
+        record_entry:       $ => seq($.key, $.value),
+        key:                $ => $._str_key,
+        value:              $ => choice($._str_any, $.record, $.list),
         
-        list:           $ => seq('[', optional(separated1($, $._list_element)), ']'),
-        _list_element:  $ => choice($._str_any, $.record, $.list),
+        list:               $ => seq('[', optional(separated1($._list_element, $._separator)), ']'),
+        _list_element:      $ => choice($._str_any, $.record, $.list),
 
         comment:            $ => token(seq(';', /.*/)),
-        _linebreak:         $ => token(/\r?\n|\r|\n|\u2028|\u2029/),
-        _logical_linebreak: $ => choice($.comment, $._linebreak),
-        _separator:         $ => choice(',', $._logical_linebreak),
+        _linebreak:         $ => /\r?\n|\r|\n|\u2028|\u2029/,
+        _separator:         $ => choice(',', $.comment, $._linebreak),
 
         str_unquoted:       $ => /[^\s\[\]\{\}";,>]+/,
         str_quoted:         $ => /"[^"]*"/,
 
         _str_key:           $ => choice($.str_unquoted, $.str_quoted),
-        _str_any:           $ => $._str_key,
+        _str_any:           $ => choice($.str_unquoted, $.str_quoted),
     }
 });
 
-function separated1($, prod) {
-    return seq(prod, repeat(seq($._separator, prod)), optional($._separator));
+function separated1(prod, sep) {
+    return seq(prod, repeat(seq(sep, prod)), optional(sep));
 }
